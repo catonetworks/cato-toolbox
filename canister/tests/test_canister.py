5a3783cafab6149c8cf956e7e890d5f1b54c06f3
Peter Lee <peter.lee@catonetworks.com>
2025-01-13 08:47:59 +0000

    CLI

--- canister/tests/test_canister.py
+++ canister/tests/test_canister.py
@@ -16,28 +16,3 @@ class InitTests(unittest.TestCase):
 	def test_default_load(self):
 		C = canister.Canister()
 		self.assertEqual(C.version, "1.0")
-		self.assertEqual(C.config["set_url"], "https://d2abwe599gw1ne.cloudfront.net/websets")
-
-
-	def test_custom_load(self):
-		good_config = {
-			"set_url": "XXX",
-		}
-		bad_config_extra = {
-			"set_url": "XXX",
-			"extra_field": "YYY",
-		}
-		bad_config_missing = {
-			"set_urXXX": "YYY",
-		}
-		#
-		# Good load, check inputs
-		#
-		C = canister.Canister()
-		self.assertEqual(C.version, "1.0")
-		self.assertEqual(C.config["set_url"], "https://d2abwe599gw1ne.cloudfront.net/websets")
-		#
-		# Extra config
-		#		
-		self.assertRaises(ValueError, canister.Canister, {"config":bad_config_extra})
-		self.assertRaises(ValueError, canister.Canister, {"config":bad_config_missing})
