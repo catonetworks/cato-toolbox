ab0a086f36c79c322073a2a57f3851d9114af327
Peter Lee <peter.lee@catonetworks.com>
2025-01-13 08:47:23 +0000

    CLI

--- canister/canister.py
+++ canister/canister.py
@@ -2,8 +2,15 @@
 # canister/canister.py
 #
 # Defines the Canister application class. Users wishing to drive Canister
-# programmatically will create one of these.
+# programmatically can create one of these. The module can also be called
+# from the command line as a no-code option.
 #
+
+import sys
+
+import canisterset
+from logger import Logger
+
 
 
 
@@ -19,33 +26,52 @@ class Canister:
 	version = "1.0"
 
 
-	def __init__(self, config=None):
-		#
-		# Parameters:
-		# ----------
-		#
-		# config
-		# 	- Dictionary containing configuration strings. If None, use
-		#     the defaults which are:
-		#       set_url: https://d2abwe599gw1ne.cloudfront.net/websets
-		#
-		if config is None:
-			self.config = {
-				"set_url": "https://d2abwe599gw1ne.cloudfront.net/websets",
-			}
+	def __init__(self):
+		pass
+
+
+#
+# The main() function allows us to call this module from the command line
+# with no additional scripting required, specifying the server IP and port
+# for the target (catcher).
+#
+
+def main():
+	#
+	# Example command line would be:
+	# 
+	# python3 -m canister <server_ip> <server_port>
+	#
+	# server_ip: the IP or host of the catcher.
+	# server_port: the port the catcher is listening on for web attacks.
+	#
+
+	#
+	# Process the CLI
+	#
+	if len(sys.argv) < 3:
+		print("Error: not enough arguments. Both the server IP and port must be specified.")
+		print("For example: python3 -m canister 192.168.1.1 8443")
+		sys.exit(1)
+	ip = sys.argv[1]
+	port = int(sys.argv[2])
+
+	#
+	# Create a Canister set for the specified target,
+	# load the default set and execute.
+	#
+	Logger.level=0
+	S = canisterset.CanisterSet(target=ip, port=port)
+	S.load(canisterset.DEFAULT)
+	S.execute()
+	for T in S:
+		if T.success:
+			result = "Pass"
 		else:
-			self.config = config
-		self.check_config()
+			result = "Fail"
+		print(f'{T.name:<40} {result}')
 
 
-	def check_config(self):
-		#
-		# Sanity check the config, looking for missing and extra fields.
-		#
-		required_fields = ["set_url"]
-		for k,v in self.config.items():
-			if k not in required_fields:
-				raise ValueError(f'Unexpected field in Canister config: {k}')
-		for field in required_fields:
-			if field not in self.config:
-				raise ValueError(f'Missing field from Canister config: {field}')
+
+if __name__ == "__main__":
+	main()
