7579bb74705abc73b6572132c505595de80da41c
Peter Lee <peter.lee@catonetworks.com>
2025-02-05 14:37:38 +0000

    CLI

--- canister/canister.py
+++ canister/canister.py
@@ -26,8 +26,57 @@ class Canister:
 	version = "1.0"
 
 
-	def __init__(self):
-		pass
+	def __init__(self, ip, port):
+		#
+		# Initialise the Canister client object.
+		#
+		# Parameters:
+		# ----------
+		#
+		# ip 	The IP address of the target for reflected tests
+		# port 	The port of the reflected target
+		#
+		self.canister_set = canisterset.CanisterSet(target=ip, port=port)
+
+
+	def load_object(self, cset):
+		#
+		# Loads a CanisterSet object
+		#
+		# Parameters:
+		# ----------
+		# cset 	A CanisterSet as a list of dictionaries.
+		#       See canisterset.py for documentation.
+		#
+		# Returns:
+		# -------
+		# (success, reasons)
+		#
+		# success	True if the load was successful, otherwise False.
+		#
+		# reasons	If the load was false, self.canister_set.errors will
+		# 			contain a list of strings indicating the cause(s).
+		#
+		reasons = self.canister_set.load(cset)
+		if len(reasons) > 0:
+			return False, reasons
+		else:
+			return True, reasons
+
+
+	def execute(self):
+		#
+		# Executes the loaded set.
+		#
+		self.canister_set.execute()
+
+
+	def results(self):
+		#
+		# Returns the results of the last run.
+		#
+		return self.canister_set.results()
+
 
 
 #
@@ -57,19 +106,50 @@ def main():
 	port = int(sys.argv[2])
 
 	#
-	# Create a Canister set for the specified target,
-	# load the default set and execute.
+	# Initialise the logger
+	# 
+	def NicePrint(level, text):
+		print("*", text)
+	Logger.level=1
+	Logger.print_output = False
+	Logger.callback = NicePrint
+
 	#
-	Logger.level=0
-	S = canisterset.CanisterSet(target=ip, port=port)
-	S.load(canisterset.DEFAULT)
-	S.execute()
-	for T in S:
+	# Create the object
+	#
+	C = Canister(ip, port)
+
+	#
+	# Print the welcome
+	#
+	print(f'Canister v{C.version} loading default test set')
+
+	#
+	# Load the default set
+	#
+	C.load_object(canisterset.DEFAULT)
+	print(f'Loaded {len(C.canister_set)} tests, executing:')
+
+	#
+	# Execute with logging 
+	#
+	C.execute()
+
+	#
+	# Print the results
+	#
+	print("\nResults:\n-------")
+	for T in C.canister_set:
 		if T.success:
 			result = "Pass"
 		else:
 			result = "Fail"
 		print(f'{T.name:<40} {result}')
+	results = C.results()
+	print("")
+	print(f'Total:{results["total"]}\
+ Succeeded:{results["succeeded"]} ({results["succeeded"]*100/results["total"]:.1f}%)\
+ Failed:{results["failed"]} ({results["failed"]*100/results["total"]:.1f}%)')
 
 
 
